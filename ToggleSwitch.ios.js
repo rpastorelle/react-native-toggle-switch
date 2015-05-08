381688d82971b75be7fcc0d54f9a681f86965cf5
Pasto <rpastorelle@yahoo.com>
2015-05-08 11:54:52 -0400

    Add basic component

---
+++ ToggleSwitch.ios.js
@@ -0,0 +1,24 @@
+'use strict';
+var React = require('react-native');
+var {
+  View,
+  Text,
+  //PropTypes,
+} = React;
+
+var ToggleSwitch = React.createClass({
+
+  //propTypes: {},
+  //getDefaultProps() {},
+
+  render() {
+    return (
+      <View>
+        <Text>ToggleSwitch</Text>
+      </View>
+    );
+  },
+
+});
+
+module.exports = ToggleSwitch;
